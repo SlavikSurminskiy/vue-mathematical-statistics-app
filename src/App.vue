8a142e4d394d5409fe1102c763f3a8a15d795fed
Surminskiy <slaviksyrminskiy@gmail.com>
2019-12-25 23:23:38 +0200

    add Vuetify

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,52 @@
 <template>
-  <div id="app">
-  </div>
+  <v-app id="inspire">
+    <v-navigation-drawer
+      v-model="drawer"
+      app
+    >
+      <v-list dense>
+        <v-list-item link to="/">
+          <v-list-item-action>
+            <v-icon>mdi-home</v-icon>
+          </v-list-item-action>
+          <v-list-item-content>
+            <v-list-item-title>Home</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+        <v-list-item link to="/about">
+          <v-list-item-action>
+            <v-icon>mdi-math-compass</v-icon>
+          </v-list-item-action>
+          <v-list-item-content>
+            <v-list-item-title>About</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+
+    <v-app-bar app color="indigo" dark>
+      <v-app-bar-nav-icon @click.stop="drawer = !drawer" />
+      <v-toolbar-title>TIMS</v-toolbar-title>
+    </v-app-bar>
+
+    <v-content>
+      <v-container class="fill-height" fluid>
+        <router-view />
+      </v-container>
+    </v-content>
+    <v-footer color="indigo" app>
+      <span class="white--text">Vyacheslav Surminskiy &copy; 2019</span>
+    </v-footer>
+  </v-app>
 </template>
 
-<style lang="scss">
-</style>
+<script>
+export default {
+  props: {
+    source: String,
+  },
+  data: () => ({
+    drawer: null,
+  }),
+};
+</script>
---
+++ src/views/About.vue
@@ -0,0 +1,9 @@
+<template>
+  <h1>About component</h1>
+</template>
+
+<script>
+export default {
+  name: 'about',
+};
+</script>
